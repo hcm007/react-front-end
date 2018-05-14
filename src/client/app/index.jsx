1e4d0e864d0debfb6e6c7f305a972333dc7ff4c3
Hongchao Ma <hongchaoma@TO-SL-NBROOMHALL.local>
2018-05-14 17:44:59 -0400

    Modified the index.jsx but I did not organized the style

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,7 +2,40 @@
 
 import React from 'react';
 import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx'
+
+
+
+const element = (
+    <div tabIndex="0">
+        <h1>Hello, luqing</h1>
+    </div>
+);
+
+const h1 = (
+
+    <h1 tabIndex="2"> wo my god</h1>
+
+);
+
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+
+    ReactDOM.render(element, document.getElementById('time'));
+
+}
+
+setInterval(tick,1000);
+
+
+
 
 class  App extends React.Component{
     render(){
@@ -18,5 +51,106 @@ class  App extends React.Component{
 }
 
 render(<App/>, document.getElementById('app'));
+
+ReactDOM.render(element, document.getElementById('root'));
+
+ReactDOM.render(h1,document.getElementById('h1'));
+
+
+
+function Welcome(pros){
+    return <h1>Hello,{pros.name}</h1>;
+}
+const welcome = <Welcome name="Sara" />;
+
+ReactDOM.render(
+    welcome,
+    document.getElementById('welcome')
+);
+
+
+
+class Clock extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {date: new Date()};
+
+    }
+
+
+    componentDidMount(){
+        this.timerID =setInterval(
+            () => this.tick(),
+            1000
+        );
+
+    }
+
+
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+
+    }
+
+
+    tick(){
+
+        this.setState({
+            date: new Date()
+        });
+    }
+
+
+
+
+    render(){
+        return(
+            <div>
+                <h1> Hello, world!</h1>
+                <h2> It is {this.state.date.toLocaleString()}.</h2>
+            </div>
+
+        );
+    }
+
+}
+
+ReactDOM.render(
+  <Clock />,
+    document.getElementById('clock')
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
